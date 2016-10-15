6e4564a6768e43212be3c6907985b950aa6e483e
geekhardik <hardik.p.shah@sjsu.edu>
2016-10-15 02:30:20 -0700

    a good working flow till end

--- public/angularjs/item.js
+++ public/angularjs/item.js
@@ -20,28 +20,42 @@ app.controller('item',function($scope,$http,$window){
 			alert("somthing's wrong in callback of item.js");
 		}
 	});	
-}
+};
+	
+	/*if($scope.qty > $scope.x.qty){
+		alert("You have exceeded qty that seller has listed for sale!");
+	}*/
+	
+
+	
 	$scope.cart = function(x){	
+		var a = Number($scope.qty);
+		var b = Number(x.qty);
+		if(a > b){
+			alert("You have exceeded qty that seller has listed for sale!");
+			
+		}else{	
 		
-		$http({			
-			method: "POST",
-			url : '/cart',
-			data : {
-				"obj" : x,
-				"qty" : $scope.qty
-			}
-						
-		}).success(function(data){
-			if (data.success == 200) {
-				console.log("done");
-				window.location.assign('cart');
+			$http({			
+				method: "POST",
+				url : '/cart',
+				data : {
+					"obj" : x,
+					"qty" : $scope.qty
+				}
 							
-			}else{
-				alert("Please sign-in first!");
-				window.location.assign('signin');
-				
-			}
-		});
+			}).success(function(data){
+				if (data.success == 200) {
+					console.log("done");
+					window.location.assign('cart');
+								
+				}else{
+					alert("Please sign-in first!");
+					window.location.assign('signin');
+					
+				}
+			});
+		}
 	}
 	
 });
